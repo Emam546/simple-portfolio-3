94a32782383d2c2583203409a6260acda92d6243
Emam546 <emam54637@gmail.com>
2023-01-20 23:25:25 +0200

    end-gallery

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import "./special-header.css"
 import Articles from "./articles"
 import Header from "./header"
 import Landing from "./landing"
+import Gallery from "./gallery"
 function App() {
   return <>
     <Header />
     <main>
       <Landing />
       <Articles />
+      <Gallery />
     </main>
   </>
 }
